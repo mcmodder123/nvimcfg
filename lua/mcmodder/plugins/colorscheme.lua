f093017ba5e1ad5c9c17d0ad8a3a4a3f7ef98626
mcmodder123 <119756788+mcmodder123@users.noreply.github.com>
2025-05-13 17:23:14 -0500

    Now uses catppuccin theme

--- lua/mcmodder/plugins/colorscheme.lua
+++ lua/mcmodder/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
-return{
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme gruvbox")
+return {
+	"catppuccin/nvim",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme catppuccin")
 
-    require("gruvbox")
-  end
+		require("catppuccin")
+	end,
 }
